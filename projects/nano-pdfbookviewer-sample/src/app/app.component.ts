9a5d018369e86306882f0ae2b140d4e320b7849d
NanoSpicer <miquelrc95@outlook.es>
2021-11-13 16:24:02 +0100

    Consumes dependency from dist/ folder

--- projects/nano-pdfbookviewer-sample/src/app/app.component.ts
+++ projects/nano-pdfbookviewer-sample/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'nano-pdfbookviewer-sample';
+export class AppComponent implements OnInit {
+  label = ''
+  pdfSource = 'http://localhost:4000/compressed.tracemonkey-pldi-09.pdf'
+  
+  ngOnInit() {}
+
+  onPdfLoadingState(isLoading$: any) {
+    const isLoading = isLoading$ as boolean;
+    this.label = isLoading ? 'LOADING' : 'NOT LOADING'
+  }
 }
--- projects/nano-pdfbookviewer-sample/src/app/app.module.ts
+++ projects/nano-pdfbookviewer-sample/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NanoPdfBookViewerModule } from 'nano-pdf-book-viewer';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NanoPdfBookViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]

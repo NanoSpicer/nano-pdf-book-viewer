6f88ae6db5e5ba15d4050913bb4a3ac8cade45a8
NanoSpicer <miquelrc95@outlook.es>
2021-11-13 16:46:05 +0100

    Fix: Import common module to be able to use *ngFor

--- projects/nano-pdf-book-viewer/src/lib/nano-pdf-book-viewer.module.ts
+++ projects/nano-pdf-book-viewer/src/lib/nano-pdf-book-viewer.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NanoPdfBookViewerComponent } from './nano-pdf-book-viewer.component';
 
@@ -8,6 +9,7 @@ import { NanoPdfBookViewerComponent } from './nano-pdf-book-viewer.component';
     NanoPdfBookViewerComponent
   ],
   imports: [
+    CommonModule
   ],
   exports: [
     NanoPdfBookViewerComponent

c93a827725d7069db37cc4c0a64a029527f5cf72
NanoSpicer <miquelrc95@outlook.es>
2021-11-13 16:56:52 +0100

    Adds sample server serving pdfs

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+app.use(express.static('.'))
+app.listen(4000, () => console.log('Listening @ 4000'))

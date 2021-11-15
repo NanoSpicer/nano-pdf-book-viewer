75cefbd7e6f157e1f4d1ae215e5e2bc5c64f9ee9
NanoSpicer <miquelrc95@outlook.es>
2021-11-15 14:49:45 +0100

    Adds toggling between pdf sources

--- projects/nano-pdfbookviewer-sample/src/app/app.component.ts
+++ projects/nano-pdfbookviewer-sample/src/app/app.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+
+type TogglePdfState = 'FIRST' | 'SECOND'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   label = ''
-  pdfSource = 'http://localhost:4000/compressed.tracemonkey-pldi-09.pdf'
-  
-  ngOnInit() {}
+  toggleDisabled = false
+  get pdfSource() {
+    const host = 'http://localhost:4000'
+    switch(this.toggleState) {
+      case 'FIRST': return `${host}/compressed.tracemonkey-pldi-09.pdf`
+      default: return `${host}/keyboard-shortcuts-macos.pdf`
+    }
+  }
+  private toggleState: TogglePdfState = 'FIRST'
 
   onPdfLoadingState(isLoading$: any) {
     const isLoading = isLoading$ as boolean;
+    this.toggleDisabled = isLoading === true // disable button if loading
     this.label = isLoading ? 'LOADING' : 'NOT LOADING'
+  }
+
+  onToggle() {
+    this.toggleState = this.toggleState === 'FIRST' ? 'SECOND' : 'FIRST'
   }
 }
